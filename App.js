7e1b044a2ffe88d336b81b3d0f95889d65b1b8e1
LalCelik <114354209+LalCelik@users.noreply.github.com>
2024-04-02 06:28:47 -0400

    h

--- App.js
+++ App.js
@@ -5,12 +5,14 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
+import Hello from "./Hello.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 
 CourseRoutes(app);
+Hello(app);
 Lab5(app);
 app.listen(process.env.PORT || 4000);
 

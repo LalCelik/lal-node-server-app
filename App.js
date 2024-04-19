611c7cbb1b74f02fa59b1b924302945bc6fb665a
LalCelik <114354209+LalCelik@users.noreply.github.com>
2024-04-19 01:54:09 -0400

    quizzes showing up

--- App.js
+++ App.js
@@ -1,65 +1,18 @@
-//new
-import "dotenv/config";
 
 import express from "express";
 import Lab5 from "./Lab5.js";
-import UserRoutes from "./Users/routes.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import QuizRoutes from "./Kanbas/quizzes/routes.js";
 
-import mongoose from "mongoose";
-
 import cors from "cors";
 import Hello from "./Hello.js";
 
-//new
-import session from "express-session";
-
-//mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
-
-
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
-mongoose.connect(CONNECTION_STRING);
-
-
 const app = express();
-app.use(
-    cors({
-      credentials: true,
-      //origin: "http://localhost:3000",
-      origin: process.env.FRONTEND_URL
-    })
-   );   
 app.use(express.json());
-
-//new
-//const sessionOptions = {
-//    secret: "any string",
-//    resave: false,
-//    saveUninitialized: false,
-//  };
- // app.use(
-//    session(sessionOptions)
- // );
-
- const sessionOptions = {
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  };
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.HTTP_SERVER_DOMAIN,
-    };
-  }  
-  app.use(session(sessionOptions));
+app.use(cors());
 
 
-UserRoutes(app);
 ModuleRoutes(app);
 
 QuizRoutes(app);
@@ -67,7 +20,5 @@ QuizRoutes(app);
 CourseRoutes(app);
 Hello(app);
 Lab5(app);
-
-const port = process.env.PORT || 4000;
 app.listen(process.env.PORT || 4000);
 

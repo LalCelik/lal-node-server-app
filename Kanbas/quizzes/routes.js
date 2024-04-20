6a9a9c4add4701f419270e1bd1d0ee24d3e90450
LalCelik <114354209+LalCelik@users.noreply.github.com>
2024-04-20 15:21:01 -0400

    aaa

--- Kanbas/quizzes/routes.js
+++ Kanbas/quizzes/routes.js
@@ -1,7 +1,6 @@
 import db from "../Database/index.js";
 
 function QuizRoutes(app) {
-
     app.put("/api/quizzes/:qid", (req, res) => {
         const { qid } = req.params;
         const quizIndex = db.quizzes.findIndex(
@@ -12,7 +11,6 @@ function QuizRoutes(app) {
         };
         res.sendStatus(204);
     });
-
 
     app.delete("/api/quizzes/:qid", (req, res) => {
         const { qid } = req.params;
@@ -37,5 +35,36 @@ function QuizRoutes(app) {
             .filter((q) => q.course === cid);
         res.send(quizzes);
     });
+
+    //get only quiz based on quiz id
+    app.get("/api/quizzes/:qid", (req, res) => {
+        const { qid } = req.params;
+        const quiz = Database.quizzes
+          .find((q) => q._id === qid);
+        if (!quiz) {
+          res.status(404).send("Course not found");
+          return;
+        }
+        res.send(quiz);
+      });
+
+      app.delete("/api/quizzes/:qid", (req, res) => {
+        const { qid } = req.params;
+        Database.quizzes = Database.quizzes
+          .filter((q) => q._id !== qid);
+        res.sendStatus(204);
+      });    
+    
+    app.post("/api/quizzes", (req, res) => {
+        const quiz = { ...req.body,
+          _id: new Date().getTime().toString() };
+        Database.quizzes.push(quiz);
+        res.send(quiz);
+      });
+    
+  app.get("/api/quizzes", (req, res) => {
+    const quizzes = Database.quizzes;
+    res.send(quizzes);
+  });
 }
 export default QuizRoutes;
